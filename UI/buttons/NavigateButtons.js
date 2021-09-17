ad8c26709d3fb43f7a015687fdb0db44f56eed38
maxi2594 <maxi2594@hotmail.com>
2021-09-17 20:43:42 -0300

    fix Pokelist loadind

---
+++ UI/buttons/NavigateButtons.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export const NavigateButtons = ({
+    handleSetUrL,
+    next,
+    previous
+}) => {
+
+    const styles = {
+        container: {
+            display: 'flex',
+            alignItems : 'center',
+            backgroundColor: 'rgb(43,47,51)',
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            marginTop: '1rem',
+            marginBottom: '2rem',
+        },
+        button:{
+            border: '2px solid grey',
+            backgroundColor: 'transparent',
+            marginLeft: '10px',
+            marginRight: '10px',
+            color: 'grey',
+            width: '5rem',
+            height: '2rem',
+        }
+    }
+    return (
+        <div style={ styles.container }>
+
+            <button 
+                style={ styles.button }
+                onClick={ ()=>handleSetUrL(previous) }
+            > 
+                PREVIOS 
+            </button>
+
+            <button 
+                style={ styles.button }
+                onClick={ ()=>handleSetUrL(next) }
+            >
+                NEXT 
+            </button>
+
+        </div>
+    )
+}
---
+++ pokemons/PokeCard.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { useFetchPokes } from '../../hooks/useFetchPokes';
+import { LoadIcon } from '../UI/loading/LoadIcon';
+
+export const PokeCard = ({ pokemon, sprites }) => {
+    
+    const styles = {
+        cardCenter: {
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            backgroundColor: 'rgb(63,67,71)',
+            borderRadius: '100px'
+        },
+        cardStyles: {
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            border: '2px solid grey',
+            backgroundColor: '#FAFAFA',
+            maxWidth: '400px',
+            margin: '5px',
+            padding: '10px'
+        },
+        cardImg: {
+            width: '150px',
+            height: '150px',
+        }
+    }
+
+    return (
+        
+        <div className="card col-sm-6 mb-2" style={ styles.cardStyles} >
+            <div className="col-md-6">
+                <div style={styles.cardCenter}>
+
+                    <img
+                        style={ styles.cardImg}
+                        src={ sprites.front_default } 
+                        className="card-img"
+                        alt={ pokemon.name }/>
+
+                </div>
+            </div>
+            <div className="col-md-8">
+                <div className="card-body">
+
+                    <h2
+                        style={{textAlign: 'center'}}
+                    > { pokemon.name } 
+                    </h2>
+
+                    <hr></hr>    
+                </div>
+            </div>
+        </div>
+)
+}
+
