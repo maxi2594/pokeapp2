02cc99ca3d304e7c5f6ea9cff2ea4788f3b444ad
maxi2594 <maxi2594@hotmail.com>
2021-11-04 19:00:13 -0300

    characterScreen test modified

--- src/test/components/pokemons/characterScreen/CharacterScreen.test.js
+++ src/test/components/pokemons/characterScreen/CharacterScreen.test.js
@@ -1,12 +1,18 @@
-import { render, screen, act, cleanup } from "@testing-library/react";
-import { shallow } from "enzyme";
+import { render, screen, cleanup, waitFor} from "@testing-library/react";
+import { shallow, ShallowWrapper } from "enzyme";
 import { MemoryRouter } from "react-router";
 import { CharacterScreen } from "../../../../components/pokemons/characterScreen/CharacterScreen"
 import { data } from "../../../fakeData/fakeData";
+import { useParams } from 'react-router'
+import { act } from "react-dom/test-utils";
+import { ShallowRenderer } from "react-test-renderer/shallow";
+
+jest.mock('react-router', () => ({
+    useParams: () => ({ id : 1})
+}));
+
 
 describe('test in <CharacterScreen/>', () => {
-
-
 
     beforeEach(() => {
         global.fetch = jest.fn(() =>
@@ -25,31 +31,34 @@ describe('test in <CharacterScreen/>', () => {
 
     test('should show properly', async () => {
 
-        const wrapper = shallow(
-            <MemoryRouter>
+        const wrapper = render(
+            //<MemoryRouter>
                 <CharacterScreen />
-            </MemoryRouter>
+            //</MemoryRouter>
         )
 
         // expect(wrapper).toMatchSnapshot();
-        expect(wrapper.find('div').length).toEqual(0);
+        // expect(wrapper.find('div').length).toEqual(0);
+        expect(wrapper.container.childNodes.length).toBe(0)
     })
 
-    test('should have content after state update', (done) => {
+    test('should have content after state update',  async (done) => {
+        // const container = document.createElement("div");
+        // document.body.appendChild(container);
 
-        const wrapper = shallow(
-            <MemoryRouter>
+        // const renderer = new ShallowRenderer()
+        
+        act(()=> {
+            
+            const continer = shallow(
+            //<MemoryRouter>
                 <CharacterScreen />
-            </MemoryRouter>
-        )
-
-        setTimeout(() => {
-            wrapper.update()
-
-            console.log(wrapper.find('ImageContainer').exists()).toBe(true);
-
-            done()
-        });
+            //</MemoryRouter>
+            )
+            console.log(continer.debug());
+        })
+        
+        // expect(container.querySelector('img')).toBeInTheDocument()
     })
 
 })
